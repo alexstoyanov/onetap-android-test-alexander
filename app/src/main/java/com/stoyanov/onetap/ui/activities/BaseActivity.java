bab58c1b97aa87a120aee799858dcff1213743fd
Alexander Stoyanov <alxndrstoyanov@gmail.com>
2016-06-05 23:45:36 +0300

    Added fused location provider check

--- app/src/main/java/com/stoyanov/onetap/ui/activities/BaseActivity.java
+++ app/src/main/java/com/stoyanov/onetap/ui/activities/BaseActivity.java
@@ -1,6 +1,9 @@
 package com.stoyanov.onetap.ui.activities;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -14,9 +17,16 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.stoyanov.onetap.R;
 import com.stoyanov.onetap.networking.events.OnForecastLoadedEvent;
 import com.stoyanov.onetap.networking.events.OnLocationChangedEvent;
@@ -38,6 +48,7 @@ public abstract class BaseActivity extends AppCompatActivity
     private static final String IS_REQUESTING_LOCATION_UPDATES_KEY = "IsRequestingLocationUpdates";
     private static final String CURRENT_LOCATION_KEY = "CurrentLocation";
     public static final int PERMISSION_USE_LOCATION = 1;
+    private static final int REQUEST_CHECK_SETTINGS = 2;
 
     private GoogleApiClient googleApiClient;
     private boolean isRequestingLocationUpdates;
@@ -109,6 +120,29 @@ public abstract class BaseActivity extends AppCompatActivity
         locationRequest.setInterval(600000);
         locationRequest.setFastestInterval(300000);
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(locationRequest);
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            status.startResolutionForResult(BaseActivity.this, REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        break;
+                }
+            }
+        });
     }
 
     @Override
@@ -199,9 +233,7 @@ public abstract class BaseActivity extends AppCompatActivity
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case PERMISSION_USE_LOCATION: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    startLocationUpdates();
-                } else {
+                if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                     Toast.makeText(this,
                             R.string.location_premision_not_granted, Toast.LENGTH_SHORT).show();
                 }
@@ -210,4 +242,19 @@ public abstract class BaseActivity extends AppCompatActivity
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        break;
+                    default:
+                        break;
+                }
+                break;
+        }
+    }
 }
